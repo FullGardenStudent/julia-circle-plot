e2560c9fcb81da46fd4315ecdb6ea3be380fb332
9thGluon <101201048+9thGluon@users.noreply.github.com>
2022-04-03 09:39:07 +0530

    initial commit

--- main.jl
+++ main.jl
@@ -1,45 +1,45 @@
-using Plots
+using Plots  #load the Plots module https://docs.juliaplots.org/latest/tutorial/
 
-@userplot PlotSin
+@userplot PlotSin  # create a user recipe for plots
 @userplot PlotSinc
 
-@recipe function pl(cp::PlotSin)
-    x,i,lab = cp.args
-    label --> lab
+@recipe function pl(cp::PlotSin)                # define a recipe function for sin and cos functions
+    x,i,lab = cp.args                           # declare the arguments
+    inds = circshift(1:length(x), i-1)          # shift the array elements for alpha line transition
+    label --> lab                               # graphs settings
     ylabel --> lab
     xlabel --> "t"
     title --> lab
-    inds = circshift(1:length(x), i-1)
     seriesalpha --> range(1,0, length(x))
     dpi --> 300
     xtickfontsize --> 1
     xtickfontcolor --> "white"
-    x[inds]
+    x[inds]                                     # return value
 end
 
-@recipe function pp(cp::PlotSinc)
-    x, y,lab, i = cp.args
-    n = length(x)
-    m = length(y)
-    aspect_ratio --> 1
+@recipe function pp(cp::PlotSinc)               # define a recipe for circle
+    x, y, i = cp.args                           # declare the function artguments
+    n = length(x)                               # get the size of x and y arrays
+    inds = circshift(1:length(x), 1-i)          # shifts the array elements for alpha line transition
+    aspect_ratio --> 1                          # settings for ploting
     title --> "Circle"
-    inds = circshift(1:length(x), 1-i)
-    ind = circshift(1:length(y),1-i)
-    label --> lab
+    label --> "(sin(t),cos(t))"
     xlabel --> "sin(t)"
     ylabel --> "cos(t)"
     dpi --> 300
     seriesalpha --> range(0, 1, n)
-    x[inds], y[ind]
+    x[inds], y[inds]
 end
 
-n = 300
-t = 0:pi/200:2*pi
-x = sin.(t)
+n = 300                                         # number of frames
+t = 0:pi/200:2*pi                               # range of the cricle, 0 to 2*pi in pi/200 setps
+x = sin.(t)                                     # defining x and y for the circle
 y = cos.(t)
 
-anim = @animate for i ∈ 1:n
-    plot(plotsin(x, i*4,"sin(t)"),plotsin(y,i*4,"cos(t)"),plotsinc(x,y,"(sin(t),cos(t))",i*4, framestyle = :zerolines))
+anim = @animate for i ∈ 1:n    # @animate macro returns Animation object and runs from 1 to n, rendering every iteration to a frame
+    #define three plots for sin(t), cos(t) and circle, which will animate w.r.t i as time per frame
+    plot(plotsin(x, i*4,"sin(t)"),plotsin(y,i*4,"cos(t)"),plotsinc(x,y,i*4))
 end
 
+# creaet a gif out of the animated frames
 gif(anim, "circle_anim30.gif", fps = 30)
